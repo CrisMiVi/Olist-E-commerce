1b59abc8ed2a39433accb99b98cc585614b0f0ba
Cris <cristinamiguelvijandi@gmail.com>
2025-07-16 16:41:00 +0100

    Update 01_CLEANING_olist_geolocation_dataset.sql

--- Dev“/dev/sql/01_CLEANING_olist_geolocation_dataset.sql
+++ Dev“/dev/sql/01_CLEANING_olist_geolocation_dataset.sql
@@ -1,5 +1,9 @@
---- Eliminate 261831 duplicated values
+-- Eliminate 261831 duplicated values and safe the query as the olist_geolocation_dataset_clean table
 
 SELECT DISTINCT
-    *
+    geolocation_zip_code_prefix
+   , geolocation_lat
+   , geolocation_lng
+   , geolocation_city
+   , geolocation_state
 FROM olist_geolocation_dataset
