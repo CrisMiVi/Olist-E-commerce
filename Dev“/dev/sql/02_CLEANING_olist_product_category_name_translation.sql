4bf9af0b5f04ea1cf3876f91b09007e9ab562a28
Cris <cristinamiguelvijandi@gmail.com>
2025-07-16 16:43:12 +0100

    Create 02_CLEANING_olist_product_category_name_translation.sql

---
+++ Dev“/dev/sql/02_CLEANING_olist_product_category_name_translation.sql
@@ -0,0 +1,6 @@
+-- Rename columns and safe table as olist_product_category_name_translation_clean
+
+SELECT
+    string_field_0 AS product_category_name
+    , string_field_1 AS product_category_name_english
+FROM olist_product_category_name_translation
